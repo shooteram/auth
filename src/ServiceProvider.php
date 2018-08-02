eb38f3cc7c1517fd70510338a182fac8168b2273
shooteram <me@shooteram.fr>
2018-08-02 15:25:51 +0200

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace shooteram\Auth;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot() {}
+}
