3498a79e88ac586ad0ec78b4e4aa2e4b0299bdea
shooteram <me@shooteram.fr>
2018-09-01 18:47:44 +0200

    add routes to handle basic authentication tasks

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace shooteram\Auth;
 
-use Laravel\Passport\Passport;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider {
     /**
@@ -11,10 +12,49 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider {
      * @return void
      */
     public function boot() {
+        $this->publishes([
+            __DIR__.'/../config/cors.php' => config_path('cors.php')
+        ], 'config');
+
+        $this->defineRoutes();
+    }
+
+    protected function defineRoutes()
+    {
+        $middlewares = collect([
+            \shooteram\Auth\Http\Middleware\Cors::class,
+            $this->getThrottle(),
+            'web',
+        ]);
+
+        $namespace = '\shooteram\Auth\Http\Controllers';
+
+        Route::match($this->preflight('get'), 'csrf', "$namespace\DefaultController@getCsrfToken")
+            ->middleware($middlewares->all());
+
+        $options = [
+            'prefix' => 'auth',
+            'middleware' => $middlewares->all(),
+            'namespace' => "$namespace\Auth",
+        ];
+
+        Route::group($options, function () use ($middlewares) {
+            Route::match($this->preflight('post'), 'login', 'LoginController@login');
+            Route::match($this->preflight('post'), 'register', 'RegisterController@register');
+            Route::match($this->preflight('post'), 'logout', 'LogoutController@logout')
+                ->middleware($middlewares->merge(['auth'])->all());
+        });
+
+        Route::match($this->preflight('get'), '/api/user', function (Request $request) {
+            return $request->user();
+        })->middleware($middlewares->merge(['auth'])->all());
+    }
+
     private function preflight(string $method): array
     {
         return [$method, 'options'];
     }
+
     private function getThrottle(): string
     {
         $throttle = config('cors.throttle');
