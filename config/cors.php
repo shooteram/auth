6b57d97961cc10c01c819292b828449e7517c1b2
shooteram <me@shooteram.fr>
2018-09-01 18:48:03 +0200

    add package configuration

---
+++ config/cors.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'allowed-origins' => [],
+
+    'allowed-headers' => [
+        'X-Requested-With',
+        'X-XSRF-TOKEN',
+        'Content-Type',
+        'Accept',
+    ],
+
+    'throttle' => [
+        'rate_limit' => 10,
+        'retry_after' => 1, // expressed in minutes
+    ],
+
+    'validation' => [
+        'login-credentials' => ['email', 'password'],
+
+        'login' => [
+            'email' => 'bail|required|email|exists:users|max:255',
+            'password' => 'bail|required',
+        ],
+
+        'register' => [
+            'name' => 'bail|required|max:255',
+            'email' => 'bail|required|email|unique:users|max:255',
+            'password' => 'bail|required|confirmed',
+        ],
+
+        'password-reset' => [],
+    ],
+];
